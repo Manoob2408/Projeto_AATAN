2aa0b08389cdae3490763067ebe011b25f0ce983
Alexandre Estevam <alexandre.estevam@going2.com.br>
2021-05-21 08:18:00 -0300

    feat: add styled-components

--- pages/index.tsx
+++
@@ -1,9 +0,0 @@
-import styles from '../styles/Home.module.css'
-
-export default function Home () {
-  return (
-    <div className={styles.container}>
-      Hello World
-    </div>
-  )
-}
---
+++ src/pages/_app.tsx
@@ -0,0 +1,13 @@
+import type { AppProps } from 'next/app'
+
+import GlobalStyle from '../styles/global'
+
+function MyApp ({ Component, pageProps }: AppProps) {
+  return (
+    <>
+      <Component {...pageProps} />
+      <GlobalStyle />
+    </>
+  )
+}
+export default MyApp
