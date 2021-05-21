0f59762d59e931389fb4809fec3ec828f666f158
Alexandre Estevam <alexandre.estevam@going2.com.br>
2021-05-21 08:06:40 -0300

    chore: add eslint

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,9 @@
-import Head from 'next/head'
 import styles from '../styles/Home.module.css'
 
-export default function Home() {
+export default function Home () {
   return (
     <div className={styles.container}>
-      Hello World    
+      Hello World
     </div>
   )
 }
