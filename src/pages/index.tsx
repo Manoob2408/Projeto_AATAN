d995c5f05257cf3d0949a3177a90f86789590e0a
Alexandre Estevam <alexandre.estevam@going2.com.br>
2021-05-21 08:47:54 -0300

    feat: add logo to splashscreen page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,14 @@
+import Image from 'next/image'
+import { Container } from '../styles/pages/Home'
+
 export default function Home () {
   return (
-    <div>
-      Hello World
-    </div>
+    <Container>
+      <Image
+        src="/logo.png"
+        width="212.43"
+        height="189.04"
+      />
+    </Container>
   )
 }
