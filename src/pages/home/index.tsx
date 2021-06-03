9ab5c818a1f151d69aa748d853cdd4ad1ae1036c
Alexandre Estevam <alexandre.estevam@going2.com.br>
2021-06-03 18:46:31 -0300

    feat: add menu items

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,12 +1,73 @@
+import Image from 'next/image'
+import Carousel from '../../components/Carousel'
 import Header from '../../components/Header'
-import { Carousel, Container, Highlights, Menu, TabMenu } from '../../styles/pages/home'
+import {
+  Container,
+  HeaderContainer,
+  Highlights,
+  Menu,
+  MenuItemContainer,
+  MenuItemButton,
+  MenuItemTitle,
+  TabMenu
+} from '../../styles/pages/home'
+
+interface MenuItemProps {
+  title: string
+  imageSrc: string
+  firstColorHex: string
+  secondColorHex: string
+  url: string
+}
+
+function MenuItem ({ title, imageSrc, firstColorHex, secondColorHex, url }: MenuItemProps) {
+  return (
+    <MenuItemContainer>
+      <MenuItemButton firstColorHex={firstColorHex} secondColorHex={secondColorHex}>
+        <Image src={imageSrc} width="100%" height="100%" objectFit="contain"/>
+      </MenuItemButton>
+      <MenuItemTitle>{title}</MenuItemTitle>
+    </MenuItemContainer>
+  )
+}
 
 export default function Home () {
   return (
     <Container>
-      <Header />
+      <HeaderContainer>
+        <Header />
+      </HeaderContainer>
       <Carousel />
-      <Menu />
+      <Menu>
+        <MenuItem
+          title="Animais"
+          imageSrc="/images/icons/icon-pets.png"
+          firstColorHex="#FF885A"
+          secondColorHex="#FE6347"
+          url=""
+        />
+        <MenuItem
+          title="Eventos"
+          imageSrc="/images/icons/icon-calendar-day.png"
+          firstColorHex="#71D4FE"
+          secondColorHex="#40B0F9"
+          url=""
+        />
+        <MenuItem
+          title="Doação"
+          imageSrc="/images/icons/icon-hand-holding-heart.png"
+          firstColorHex="#FEB940"
+          secondColorHex="#F99327"
+          url=""
+        />
+        <MenuItem
+          title="Voluntariado"
+          imageSrc="/images/icons/icon-users.png"
+          firstColorHex="#59E48A"
+          secondColorHex="#2AC975"
+          url=""
+        />
+      </Menu>
       <Highlights />
       <TabMenu />
     </Container>
--- src/styles/pages/Home.ts
+++ src/styles/pages/Home.ts
@@ -4,14 +4,61 @@ export const Container = styled.div`
   width: 100vw;
   height: 100vh;
   display: flex;
-  justify-content: center;
+  flex-direction: column;
   align-items: center;
 `
 
-export const Carousel = styled.div``
-
-export const Menu = styled.div``
+export const HeaderContainer = styled.div`
+  width: 100%;
+  height: 110px;
+  box-shadow: 0px 3px 6px #0000000F;
+`
 
 export const Highlights = styled.div``
 
 export const TabMenu = styled.div``
+
+export const Menu = styled.div`
+  display: flex;
+  width: 100%;
+  flex-direction: row;
+  justify-content: space-evenly;
+`
+
+export const MenuItemContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  cursor: pointer;
+
+
+  -webkit-touch-callout: none; /* iOS Safari */
+    -webkit-user-select: none; /* Safari */
+     -khtml-user-select: none; /* Konqueror HTML */
+       -moz-user-select: none; /* Old versions of Firefox */
+        -ms-user-select: none; /* Internet Explorer/Edge */
+            user-select: none; /* Non-prefixed version, currently
+                                  supported by Chrome, Edge, Opera and Firefox */
+`
+
+interface MenuItemButtonProps {
+  firstColorHex: string
+  secondColorHex: string
+}
+
+export const MenuItemButton = styled.div<MenuItemButtonProps>`
+  width: 58px;
+  height: 58px;
+  border-radius: 50%;
+  padding: 15px;
+
+  background: transparent linear-gradient(180deg, ${({ firstColorHex }) => firstColorHex} 0%, ${({ secondColorHex }) => secondColorHex} 100%) 0% 0% no-repeat padding-box;
+  box-shadow: 0px 3px 6px #0000003B;
+
+`
+
+export const MenuItemTitle = styled.div`
+  margin-top: 8px;
+  font: normal normal bold 13px/18px Open Sans;
+  color: #404E7F;
+`
